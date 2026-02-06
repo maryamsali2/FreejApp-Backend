d19b4e7c3e91e95fc70807eee6518266c22d2e55
maryamsali2 <maryamsalah233@gmail.com>
2026-02-07 00:11:36 +0300

    user schema is done

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 
 
 // User Schema 
-const UserSchema = new mongoose ({
+const UserSchema = new mongoose.Schema ({
     username:{
         type: String,
         required: true,
@@ -22,11 +22,17 @@ const UserSchema = new mongoose ({
     
     password:{
         type: String,
-        require: true,
-        minlength:[6,"Password must be at least 6 characters",,
+        required: true,
+        minlength:[6,"Password must be at least 6 characters"]
 
-        ]
-    }
+        
+    },
+
+    role:{
+        type: String,
+        enum:["costumer","owner"],
+        default:"costumer"
+    },
 },
 {
     timestamps: true 
